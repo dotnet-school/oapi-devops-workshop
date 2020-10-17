ad3c064eafda441c087cc6a68d35c275e5684ca5
nishant <nishant.singh87@gmail.com>
2020-10-17 15:42:33 +0530

    Create Controllers/HealthCheckController.cs

---
+++ OAPI.Service/Controllers/HealthCheckController.cs
@@ -0,0 +1,21 @@
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace OAPI.Service.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class HealthCheckController : ControllerBase
+    {
+       [HttpGet]
+        public IDictionary<string, object> Get()
+        {
+            return new Dictionary<string, object>()
+            {
+                            ["version"] = "1.0", 
+                            ["healthy"] = true, 
+                            ["message"] = "Up and running", 
+            };
+        }
+    }
+}
